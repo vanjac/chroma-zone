cabd3e7bc93a3fa70e349dc811d15d73fa752eee
j v <vantjac@gmail.com>
2024-11-07 22:43:20 -0800

    Change font to custom variation of Iosevka Aile

--- tools/font-subset.ps1
+++
@@ -1,5 +0,0 @@
-# https://markoskon.com/creating-font-subsets/
-# https://github.com/be5invis/Iosevka/issues/1414
-Get-ChildItem assets\woff2 | Foreach-Object {
-	pyftsubset .\assets\woff2\$_ --output-file=$_ --flavor=woff2 --layout-features-="cv##,sv##" --unicodes="U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD"
-}
