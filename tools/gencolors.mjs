6c06cff02dc19564a9bfccb94329b5afaa012c44
j v <vantjac@gmail.com>
2024-12-28 11:38:25 -0800

    Convert colors.css to UTF-8

--- tools/gencolors.mjs
+++ tools/gencolors.mjs
@@ -1,4 +1,4 @@
-'use strict'
+import * as fs from 'node:fs';
 // Generate link colors for the navigation sidebar
 
 // https://twitter.com/jon_barron/status/1388233935641976833
@@ -6,14 +6,18 @@
 // https://krazydad.com/tutorials/makecolors.php
 let numColors = process.argv[2]
 let f = x => Math.floor(Math.sin(Math.PI * x)**2 * 230).toString(16).toUpperCase().padStart(2, '0')
+let css = ''
 for (let i = 0; i < numColors; i++) {
     let h = i / numColors
     let r = f(3/6 - h), g = f(5/6 - h), b = f(7/6 - h)
     let className = '.link-color' + i
-    console.log(`${className}:hover {
+    css += `${className}:hover {
     background-color: #${r}${g}${b};
 }
 ${className}:target {
     background-color: #${r}${g}${b};
-}`)
 }
+`
+}
+
+fs.writeFileSync('colors.css', css)
