657e355485c3b7b5080e5c79c0601af14677205f
j <chroma@chroma.zone>
2025-04-17 21:32:56 -0700

    Fix quick links in index / updates

--- _pandoc/metadata-file.sh
+++ _pandoc/metadata-file.sh
@@ -2,9 +2,9 @@
 set -eu
 
 case $1 in
-  _site/index.html)
+  _site/index.frag.html)
     echo "home.yaml" ;;
-  _site/updates.html)
+  _site/updates.frag.html)
     echo "updates.yaml" ;;
   _site/WingEd/README.md)
     echo "winged.yaml" ;;
