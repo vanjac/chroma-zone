ecae2729f6cda3b504d22daaf4086b43278ef089
j <chroma@chroma.zone>
2025-04-19 16:01:02 -0700

    Only show links for README

--- _pandoc/file-defaults.sh
+++ _pandoc/file-defaults.sh
@@ -8,25 +8,25 @@ case "$1" in
     echo "home.yaml" ;;
   _site/updates.frag.html)
     echo "updates.yaml" ;;
-  _site/WingEd/README.md)
+  _site/winged/README.md)
     echo "winged.yaml" ;;
-  _site/aodix-enhanced/*)
+  _site/aodix-enhanced/README.md)
     echo "aodix-enhanced.yaml" ;;
-  _site/aodix-repair/*)
+  _site/aodix-repair/README.md)
     echo "aodix-repair.yaml" ;;
-  _site/ar-recorder/*)
+  _site/ar-recorder/README.md)
     echo "ar-recorder.yaml" ;;
-  _site/chromafiler/*)
+  _site/chromafiler/README.md)
     echo "chromafiler.yaml" ;;
-  _site/riscy-save/*)
+  _site/riscy-save/README.md)
     echo "riscy-save.yaml" ;;
-  _site/spans/*)
+  _site/spans/README.md)
     echo "spans.yaml" ;;
-  _site/su-extensions/*)
+  _site/su-extensions/README.md)
     echo "su-extensions.yaml" ;;
-  _site/tapedeck/*)
+  _site/tapedeck/README.md)
     echo "tapedeck.yaml" ;;
-  _site/nspace/*)
+  _site/nspace/README.md)
     echo "nspace.yaml" ;;
   *)
     echo "empty.yaml" ;;
